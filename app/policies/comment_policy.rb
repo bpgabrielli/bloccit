675125a99cbf0d7a4fc6c2095b73943f37aa3996
bpgabrielli <bpgabrielli@gmail.com>
2014-11-08 19:10:37 +0000

    Added topic/post/comment destruction

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -1,7 +1,5 @@
-class PostPolicy < ApplicationPolicy
-
+class CommentPolicy < ApplicationPolicy
   def destroy?
     user.present? && (record.user == user || user.admin? || user.moderator?)
   end
-
 end
