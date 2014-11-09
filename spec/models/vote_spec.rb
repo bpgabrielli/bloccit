27de75e1a93e7c93d25007ffc88efe6e446db3d3
bpgabrielli <bpgabrielli@gmail.com>
2014-11-09 12:42:41 -0500

    Midway thru checkpoint, need to merge the reroutes

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,7 +1,8 @@
+require 'rails_helper'
+
 describe Vote do 
   describe "validations" do
     describe "value validation" do
-      it "only allows -1 or 1 as values" do
 
        before do
          @v = Vote.new(value: 1)
@@ -11,23 +12,22 @@ describe Vote do
 
        describe '1 as value' do # Or should this reference the method #valid?
          it "returns 1 as true" do
-           expect( @v.valid ).to eq(true)
+           expect( @v.valid? ).to eq(true)
          end
        end
 
        describe '-1 as value' do
          it "returns -1 as true" do
-           expect( @v2.valid ).to eq(true)
+           expect( @v2.valid? ).to eq(true)
          end
        end  
 
        describe '2 as value' do
          it "returns 2 as false" do
-           expect( @v3.valid ).to eq(false)
+           expect( @v3.valid? ).to eq(false)
          end
        end 
 
-      end
     end
   end
 end
