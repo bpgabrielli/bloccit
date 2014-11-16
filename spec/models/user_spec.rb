baf6b3ad95b66e1cab0e0d29ecc6af0cf3b7f42a
bpgabrielli <bpgabrielli@gmail.com>
2014-11-16 12:19:50 -0500

    Completed Favoriting Checkpoint

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe User do
+
+ include TestFactories
+
+ describe "#favorited(post)" do
+
+   before do
+     @post = associated_post
+     @user = authenticated_user
+   end
+
+   it "returns `nil` if the user has not favorited the post" do
+     expect( @user.favorites.find_by_post_id(@post.id) ).to be_nil
+   end
+
+   it "returns the appropriate favorite if it exists" do
+     @user.favorites.create(post: @post)
+     expect( @user.favorites.find_by_post_id(@post.id) ).not_to be_nil
+   end
+ end
+end
